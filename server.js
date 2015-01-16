e6c00ace8d32bf1f36adfd8595141b44dc512a16
t638403 <e.j.vd.hooven@gmail.com>
2015-01-16 15:25:47 +0100

    Setup express and socket.io

---
+++ server.js
@@ -0,0 +1,18 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var cfg = require('./cfg')
+
+server.listen(cfg.server.port);
+
+app.use("/", express.static(path.join(__dirname, 'public')));
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
